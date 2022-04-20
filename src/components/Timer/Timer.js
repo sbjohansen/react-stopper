8f7f8be339d28435f588259b6cd9382d6285245e
Slawomir Johansen <baran@fastmail.com>
2022-04-20 20:25:02 +0200

    ADD protection from multiple states

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -20,25 +20,32 @@ const Timer = () => {
   //let milliseconds = time.getMilliseconds()
 
 
-  let milliseconds = String(time).slice(-3);
+  let milliseconds = String(time).slice(-3).padStart(3, "0");
   let seconds = String(Math.floor(time / 1000)).padStart(2, "0");
   let minutes = String(Math.floor(seconds / 60)).padStart(2, "0");
   let hours = String(Math.floor(minutes / 60)).padStart(2, "0");
   
+  let [check, setCheck] = useState(false)
+
   const start = () => {
-    
+    if(check === false){
+    setCheck(true);
     setTimer(setInterval(() => {
       setTime(prevValue => prevValue + 1);
-    }, 1))
+    }, 1)) 
+  } 
+  
   };
 
   const stop = () => {
     clearInterval(timer);
-
+    setCheck(false);
   }
  
   const reset = () => {
-    setTime(0)
+    stop();
+    setTime(0);
+    setCheck(false);
   }
 
 
